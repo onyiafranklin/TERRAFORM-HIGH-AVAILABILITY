92d8e1e303733b5d39b4612582f5174f870e8d31
Ikechukwu Onyejekwe <iyke497@gmail.com>
2022-08-29 22:00:29 +0000

    Add subnets and AZ's

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data "aws_availability_zones" "azs" {
+	state = "available"
+}
+
 resource "aws_vpc" "main" {
 	cidr_block = var.vpc_cidr
 	enable_dns_hostnames = true
@@ -24,6 +28,26 @@ resource "aws_internet_gateway_attachment" "main_gw_attachment" {
 	vpc_id = aws_vpc.main.id
 	internet_gateway_id = aws_internet_gateway.main_gateway.id
 }
+
+// Subnets
+
+resource "aws_subnet" "public_subnet_1" {
+	cidr_block = var.public_subnet_cidr
+	vpc_id = aws_vpc.main.id
+	map_public_ip_on_launch = true
+	availability_zone = data.aws_availability_zones.azs.names[0]
+}
+
+resource "aws_subnet" "private_subnet_1" {
+	cidr_block = var.private_subnet_cidr
+	vpc_id = aws_vpc.main.id
+	availability_zone = data.aws_availability_zones.azs.names[5]
+}
+
+
+
+
+// Outputs
 
 output "igw_id" {
 	value = aws_internet_gateway.main_gateway.id

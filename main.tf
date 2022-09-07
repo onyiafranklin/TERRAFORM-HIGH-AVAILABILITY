a23e44b8bd2f5a9f6e24e2ceccb65d2d0b72e004
Ikechukwu <iyke497@gmail.com>
2022-09-07 08:59:23 +0000

    Include Route tables, rules and associations

--- main.tf
+++ main.tf
@@ -59,11 +59,93 @@ resource "aws_subnet" "private_subnet_2" {
 	availability_zone = data.aws_availability_zones.azs.names[1]
 }
 
+// NAT Gateways and EIP's
+resource "aws_eip" "nat_gateway_1_eip" {
+  vpc = true
+  
+  depends_on = [
+  	aws_internet_gateway_attachment.igw_attachment
+  ]
+}
+
+resource "aws_eip" "nat_gateway_2_eip" {
+  vpc = true
+  
+  depends_on = [
+  	aws_internet_gateway_attachment.igw_attachment
+  ]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+}
+
+resource "aws_nat_gateway" "nat_gateway_2" {
+  allocation_id = aws_eip.nat_gateway_2_eip.id
+  subnet_id     = aws_subnet.public_subnet_2.id
+}
+
+
+//Public Route Tables and Routing rules
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+//Private route tables and routing rules
+resource "aws_route_table" "private_route_table_1" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_1_rt_assoc" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table_1.id
+}
+
+
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_2.id
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_2_rt_assoc" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table_2.id
+}
 
 
 
 // Outputs
-
 output "igw_id" {
 	value = aws_internet_gateway.igw.id
 }
